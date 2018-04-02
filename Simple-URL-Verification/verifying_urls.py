7617585e829a5a8454332cd42aa60704681958c5
Besmire Thaqi <besmire@github.com>
2018-04-02 15:52:33 +0200

    Modified the code
    1. Added the part of reading from a .csv file,
    2. Modified the code for an improved result about redirect URLs and working URLs,
    3. Created the DataFrame to save the result in a .csv file

--- Simple-URL-Verification/verifying_urls.py
+++ Simple-URL-Verification/verifying_urls.py
@@ -3,40 +3,93 @@ import requests
 import socket
 import urllib
 import json
+import pandas as pd
 
 
-urls = codecs.open('file_urls.txt', 'r', 'utf-8')
+urls = codecs.open('file_urls.csv', 'r', 'utf-8')
 
-for url in urls:
-    print("__________")
+given_urls = []
+results = []
 
-    try:
-        socket.setdefaulttimeout(8000)
-        req = requests.head("http://" + url[:-2])
-        print(url)
-        print(req.status_code)
-        r = req.status_code
-        request = str(r)
-        if(request[0] == '3'):
-            print("Redirect")
-        elif(request[0] == '2'):
-             print("Working")
-        elif(request[0] == '4'):
-            print("Not working")
-        # return req.status_code
-    except urllib.error.URLError as e:
-        print(e.reason)
-    except:
-        print("Exception: our program couldn't get the IP from website")
+filename = "verified_urls.csv"
 
+try:
+    f = open(filename, "a")
+    f.seek(0)
+    f.truncate()
+except:
+    print("Please close the .csv file in order for changes to append")
 
-# print(validator.validate_website_url("e--l,34lm234--gps.nl"))rijke.biz
+def verify_urls():
+    # Going through each URL in the file
+    for url in urls:
 
-# if __name__ == '__main__':
-# 	for i in range(4):
-# 		p = mp.Process(target=verify_url)
-# 		urllist = []
-# 		urllist.append(p)
-# 		p.start()
-# 		print(p)
-# 		# print(urllist)
+        # given_urls.append(url[:-2])
+
+        print("__________")
+
+        try:
+            socket.setdefaulttimeout(8000)
+            req = requests.get("http://" + url[:-2])
+
+            # At first we print the URL
+            print(url)
+
+            print(req.status_code)
+
+            # Saving the status code in a variable to String 
+            r = req.status_code
+            request = str(r)
+
+            if(request[0] == '2'):
+                if(req.history):
+                    # This is for checking the redirect URLs
+                    for i, response in enumerate(req.history, 1):
+                        print("Redirect: ", response.url)
+                    result = "Redirect"
+                    results.append(result)
+                    given_urls.append(url[:-2])
+                else:
+                    result = "Working"
+                    print(result)
+                    results.append(result)
+                    given_urls.append(url[:-2])
+
+            elif(request[0] == '4'):
+                result = "Not working"
+                print(result)
+                results.append(result)
+                given_urls.append(url[:-2])
+
+            elif(request[0] == '5'):
+                result = "Internal server problems"
+                print(result)
+                results.append(result)
+                given_urls.append(url[:-2])
+
+            else:
+                result = " "
+                print(result)
+                # results.append(result)
+
+        except urllib.error.URLError as e:
+            print(e.reason)
+            continue
+
+        except requests.exceptions.SSLError as q:
+            print(url)
+            print("SSL Error")
+            pass
+   
+    # print(given_urls)
+    # print(results)
+
+    raw_data = {'urls': given_urls,
+                'result': results}
+
+    df = pd.DataFrame(raw_data, columns = ['urls','result'])
+    df.to_csv(f)
+
+verify_urls()
+
+f.close()
